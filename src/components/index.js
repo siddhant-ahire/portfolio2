bb82918479be385c5d7d3b9aba341d6d01945440
siddhantahireofficial <siddhantahireofficial@gmail.com>
2021-04-07 21:42:16 +0530

    home page finished and started work on resume component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Header from './Header';
 import Navbar from './Navbar';
+import Particles from "react-particles-js";
+import {makeStyles} from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    particlesCanva:{
+        position:'absolute',
+        opacity:0.3
+    }
+})
 
 const Home = () => {
+    const classes = useStyles();
     return (
         <div>
             <Navbar/>
+            <Header/>
+            <Particles
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles:{
+                        number:{
+                        value:45,
+                        density:true,
+                        value_area:900
+                        }
+                    },
+                }}
+            />
         </div>
     )
 }
